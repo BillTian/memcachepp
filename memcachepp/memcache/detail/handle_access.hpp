be17ee889de34a9c1afae085c5e589aba739a72e
BillTian <billtian945@gmail.com>
2015-05-21 12:53:00 +0800

    format copyright

--- memcachepp/memcache/detail/handle_access.hpp
+++ memcachepp/memcache/detail/handle_access.hpp
@@ -1,4 +1,4 @@
-// Copyright 2015 (c) Bill <billtian945@gmail.com>
+// Copyright 2015 (c) Bill Tian <billtian945@gmail.com>
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
